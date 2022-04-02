578b281b732590108ea05187990ebbc9df5899ef
Guilherme Dantas <68871776+Guilhermedantass@users.noreply.github.com>
2022-04-01 23:07:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import armas
 
 NOME = input('Ola aventureiro, está preparado para uma nova aventura? Antes de tudo, nos diga seu nome: ')
 
-# print('Bom {}, para encarar essa aventura, qual classe mais se identifica com vc?'.format(NOME))
-# print(''' 1 - Guerreiro
-#  2 - Ladino
-#  3 - Arqueiro
-#  4 - Mago
-# ''')
 
 eu = personagens.Guerreiro(NOME, level=2, forca=10, destreza=0, resistencia=5)
 print(eu)
